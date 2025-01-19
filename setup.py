3f8b5106243f0c8a48bcad35e85a5b9d6ea2beda
jp-96 <jp-96@example.com>
2025-01-19 20:55:50 +0900

    comments

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rptgen1',  # The name of your package
-    version='0.1.0',           # The initial release version
-    author='jp-96',        # Your name or organization
+    name='rptgen1',     # The name of your package
+    version='0.1.0',    # The initial release version
+    author='jp-96',     # Your name or organization
     author_email='jp-96@example.com',  # Your email
     description='report generator 1',  # A short description
-    long_description=open('README.md').read(),  # Long description read from README.md
-    long_description_content_type='text/markdown',  # Description content type (e.g., text/markdown)
+    long_description=open('README.md').read(),          # Long description read from README.md
+    long_description_content_type='text/markdown',      # Description content type (e.g., text/markdown)
     url='https://github.com/jp-96/report-generator-1',  # URL to your package repository
-    package_dir={'': 'src'},  # Specify the source directory
-    packages=find_packages(where='src'),  # Automatically find packages under 'src'
+    package_dir={'': 'src'},                # Specify the source directory
+    packages=find_packages(where='src'),    # Automatically find packages under 'src'
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
